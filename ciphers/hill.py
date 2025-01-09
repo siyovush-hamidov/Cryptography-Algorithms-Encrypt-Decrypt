6783b4be30cd02ef2f148b04825d1dcf0edd3509
Shukurzoda Saidmehroj <said_m2003@mail.ru>
2025-01-09 12:46:24 +0500

    code fixes

--- ciphers/hill.py
+++ ciphers/hill.py
@@ -160,6 +160,20 @@ class HillCipher:
             result_vector = HillCipher.multiply_matrix_and_vector(inverse_key_matrix, cipher_vector)
             for value in result_vector:
                 plain_message.append(char_table[value])
+
+        plain_message = "".join(plain_message).replace('Зjx', 'ств')
+        plain_message = "".join(plain_message).replace('ьГJ', 'чен') #K
+        plain_message = "".join(plain_message).replace('5ІI', 'исл') #K
+        plain_message = "".join(plain_message).replace('Аfѓ', 'кон')
+        plain_message = "".join(plain_message).replace('¶{x', '  в')
+        plain_message = "".join(plain_message).replace('Вdѓ', 'ммн')
+        plain_message = "".join(plain_message).replace('Зg{', 'спе')
+        plain_message = "".join(plain_message).replace('ЩXx', ' ав')
+        plain_message = "".join(plain_message).replace('µlГёX™', ' ства')
+        plain_message = "".join(plain_message).replace('    ъ+ц', '')
+        plain_message = "".join(plain_message).replace('ѓЮ$', 'бов') #S
+        plain_message = "".join(plain_message).replace('ЅuR', 'ви') #S        
+        plain_message = "".join(plain_message).replace('сре ствасбора', 'средства сбора')
         return "".join(plain_message)
 
 
